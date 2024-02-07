167324589b2cff193adab42f63a0ac614107179b
juli868 <bugijul@gmail.com>
2024-02-08 00:27:50 +0200

    Finishing up data management.

---
+++ web/data_manag.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Data management."""
+from management import user, airport, flight
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import sqlalchemy
+
+classes = {"User": User, "Airport": Airport, "Flight": Flight}
+
+
+class Storage:
+    """interacts with the Mysql database."""
+
+    __engine = None
+    __session = None
+
+    def __init(self):
+        FLIGHT_USER = getenv('FLIGHT_USER')
+        FLIGHT_PWD = getenv('FLIGHT_PWD')
+        FLIGHT_HOST = getenv('FLIGHT_HOST')
+        FLIGHT_DB = getenv('FLIGHT_DB')
+        FLIGHT_ENV = getenv('FLIGHT_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(FLIGHT_USER,
+                                             FLIGHT_PWD,
+                                             FLIGHT_HOST,
+                                             FLIGHT_DB))
+
+    def all(self, cls=None):
+        """Querry on the current session."""
+        new_dict = {}
+        for clss in clasess:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.sesion.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
+
+    def save(self):
+        """Save all changes made during the session."""
+        self.__session.commit()
+
+    def new(self, obj):
+        """Add a new record to the current session."""
+        self.__session.add(obj)
+
+    def delete(self, obj):
+        """Delete from the current db session obj if not none."""
+        self.__session.delete(obj)
+
+    def reload(self):
+        """Reload data from the database."""
+        Base.meatadata.create_all(self.__engine)
+        sess_factor = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factor)
+        self.__session = Session
+
+    def close(self):
+        """End session."""
+        self.__session.remove()
--- web/management.py
+++ web/management.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Integer, String
 from base_model import BaseModel
 
 
-class user(BaseModel):
+class User(BaseModel):
     """Define the attributes of a user."""
 
     table_name = 'user'
@@ -13,9 +13,44 @@ class user(BaseModel):
     mail = Column(String(120), nullable=False, unique=True)
     pwd = Column(string(60), nullable=False)
 
-    def __init__(fname, lname, mail, password):
+    def __init__(self, fname, lname, mail, password):
         """Initialise the class attributes."""
         self.f_name = fname
         self.l_name = lname
         self.mail = mail
         self.pwd = password
+
+
+class Airport(BaseModel):
+    """Define the atrributes of an airport."""
+
+    country = Column(String(60), nullable=False)
+    name = Column(String(60), nullable=False)
+    long = Column(Float, nullable=False)
+    lat = Column(Float, nullable=False)
+
+    def __init(self, country, name, latitude, longitude):
+        self.country = country
+        self.name = name
+        self.long = longitude
+        self.lat = lat
+
+
+class Flight(BaseModel):
+    """Define the attributes of a flight."""
+
+    table_name = 'flight'
+    location_fr =\
+        Column(String(60), ForeignKey('airport.name'), nullable=False)
+    location_to = Column(String(60), ForeignKey('airport.id'), nullable=False)
+    departure_time = Column(DateTime, nullable=False)
+    amount = Column(Float, nullable=False)
+    trip_class = Column(String(20), nullable=False)
+
+    def __init(self, l_from, l_to, departure, trip_class, amount):
+        """Initialise the instance."""
+        self.location_fr = l_from
+        self.location_to = l_to
+        self.departure_time = departure
+        self.amount = amount,
+        self.trip_class = trip_class
